2389c8ac96440e1cb0c3240b1d7bc97a0b37f8e8
Paranjoy Bordoloi <paranjoy123@gmail.com>
2026-01-16 16:13:14 +0530

    Day2+Assignment 2(partial)

---
+++ FN.java
@@ -0,0 +1,15 @@
+public class FN {
+    public static void main(String[] args) {
+        char[] arr = {'P','a','r','a','n','j','o','y'};
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+        }
+        
+        System.out.println(); 
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            System.out.print(arr[i]);
+        }
+    }
+}
